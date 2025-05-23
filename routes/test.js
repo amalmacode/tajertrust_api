28dd8b2731d82235adf5d3c9a0bcc42409a71c6f
AM_TAJER <111664187+amalmacode@users.noreply.github.com>
2025-05-23 20:58:39 +0100

    Update test.js

--- routes/test.js
+++ routes/test.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db'); // or correct path to db.js
+const pool = require('./db'); // or correct path to db.js
 
 router.get('/test-db', async (req, res) => {
   try {
