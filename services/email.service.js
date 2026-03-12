02a4712b45bb60ee8af7d05fab4afe9d79d9ed59
amalmacode <amaldotrading@gmail.com>
2026-03-12 13:55:29 +0000

    email service force IP4

--- services/email.service.js
+++ services/email.service.js
@@ -1,11 +1,13 @@
 
 // USE NODEMAILER FO TESTING 
 const nodemailer = require("nodemailer");
+const dns = require("dns");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family:4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
